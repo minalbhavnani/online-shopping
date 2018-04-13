a0a9812f39cd36f4ed5dfe15349269e6f846fbba
Minal Bhavnani <minalbhavnani94@gmail.com>
2018-04-13 11:28:24 -0400

    Project setup

---
+++ shoppingbackend/src/main/java/com/minal/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.minal.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
